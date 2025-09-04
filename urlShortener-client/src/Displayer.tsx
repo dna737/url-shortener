6381ecf16f2771b7dd1f4ad02164e6ae1e565516
dna737 <adnanhere27@gmail.com>
2025-09-04 00:45:53 -0500

    display shortenedUrl with copy button

--- urlShortener-client/src/Displayer.tsx
+++ urlShortener-client/src/Displayer.tsx
@@ -1,11 +1,13 @@
+import { CopyButton } from "@/components/ui/shadcn-io/copy-button";
+
 export function Displayer(props: { originalUrl: string; shortenedUrl: string }) {
   const { originalUrl, shortenedUrl } = props;
-  console.log("URL: ", originalUrl);
 
   return (
     <div>
       <h1>Original URL: {originalUrl}</h1>
-      <h1>Abridged URL: {shortenedUrl}</h1>
+      <h1>Shortened URL: {shortenedUrl}</h1>
+      <CopyButton content={shortenedUrl} variant="outline" />
     </div>
   )
 }
