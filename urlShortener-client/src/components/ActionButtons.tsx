a039cb04a053f9b6ce3788d1fcb8290d33775bd2
dna737 <adnanhere27@gmail.com>
2025-09-05 00:37:55 -0500

    modify gitignore

--- urlShortener-client/src/components/ActionButtons.tsx
+++ urlShortener-client/src/components/ActionButtons.tsx
@@ -1,5 +1,6 @@
 import { CopyButton } from "./ui/shadcn-io/copy-button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { IconLinkPlus } from '@tabler/icons-react';
 
 export default function ActionButtons(props: { shortenedUrl: string }) {
   const { shortenedUrl } = props;
@@ -9,6 +10,9 @@ export default function ActionButtons(props: { shortenedUrl: string }) {
       <TooltipTrigger asChild>
         <CopyButton content={shortenedUrl} variant="outline" />
       </TooltipTrigger>
+      <TooltipContent>
+        <p>Copy to clipboard</p>
+      </TooltipContent>
       <TooltipContent>
         <p>Copy to clipboard</p>
       </TooltipContent>
